04485906fa2b28611d8ccb32f71573c0c3521802
yamatao <serenadio@gmail.com>
2020-09-29 12:48:10 +0300

    Completed the first task: print time, ntp.time and remove trailing milliseconds. Added test.

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	// getting the host current time
+	hostTime := time.Now().Round(time.Second)
+
+	fmt.Printf("current time: %+v\n", hostTime)
+
+	// getting ntp time
+	url := "2.us.pool.ntp.org"
+
+	ntpTime, err := ntp.Time(url)
+	if err != nil {
+		log.Fatalf("failed to query time from the '%s': %v", url, err)
+	}
+	ntpTime = ntpTime.Round(time.Second)
+
+	fmt.Printf("exact time: %+v\n", ntpTime)
 }
