9d34ae2cc6d0b6f70fa1face442e98e5919c2bdb
yamatao <serenadio@gmail.com>
2020-10-17 16:22:13 +0300

    HW06: Implemented stage runner

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,46 @@ type (
 
 type Stage func(in In) (out Out)
 
-func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here
-	return nil
+func stageWrapper(outerIn In, stageIn chan interface{}, stageOut Out, outerOut chan interface{}, done In) {
+	defer close(outerOut)
+	defer close(stageIn)
+	for {
+		select {
+		// wait for something from outer scope
+		case i, ok := <-outerIn:
+			if !ok {
+				return
+			}
+			// now push the `i` to a real stage
+			select {
+			case stageIn <- i:
+				break
+			case <-done:
+				return
+			}
+			// and wait a result from the stage
+			select {
+			case o := <-stageOut:
+				outerOut <- o
+			case <-done:
+				return
+			}
+		case <-done:
+			return
+		}
+	}
+}
+
+func ExecutePipeline(in In, done In, stages ...Stage) (out Out) {
+	ch := in
+	for _, stage := range stages {
+		stageIn := make(chan interface{}) // auxiliary channels for the wrapper
+		outerOut := make(chan interface{})
+
+		stageOut := stage(stageIn)
+		go stageWrapper(ch, stageIn, stageOut, outerOut, done)
+		ch = outerOut
+	}
+	out = ch
+	return
 }
